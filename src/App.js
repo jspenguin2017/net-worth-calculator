961410f43a03cde51f1062f7ab7e7257d8711c44
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2021-01-21 19:39:59 -0700

    finish basic layout

--- src/App.js
+++ src/App.js
@@ -9,8 +9,30 @@ class Component extends React.Component {
     totalLiabilities: 908297,
   };
 
+  defaultFormatter = new Intl.NumberFormat('en', { style: 'currency', currency: 'USD' });
+
+  formatCurrency(value, currency) {
+    // Show '$' instead of 'CA$'
+    if (currency === 'CAD')
+      currency = 'USD'
+
+    const formatter = currency ? new Intl.NumberFormat('en', {style: 'currency', currency}) : this.defaultFormatter;
+
+    return formatter.format(value);
+  }
+
+  formatCurrencyNoSign(value) {
+    return this.formatCurrency(value).replace('$', '');
+  }
+
+  recalculate() {
+    // TODO
+    this.forceUpdate();
+  }
+
   handleCurrencyChange(event) {
     model.currency.changeCurrency(event.target.value);
+    this.recalculate();
   }
 
   getTable(model, name) {
@@ -19,17 +41,19 @@ class Component extends React.Component {
       rows.push(
         <tr>
           <td>{name}</td>
-          <td className="align-right">$<input value={model.getTotalValue(name)}></input></td>
+          <td className="align-right input">
+            $<input className="input" value={this.formatCurrencyNoSign(model.getTotalValue(name))}></input>
+          </td>
         </tr>
       );
     }
 
     return (
-      <table>
+      <table className="gray">
         <thead>
           <tr>
-            <th className="width80">{name}</th>
-            <th className="width20"></th>
+            <th className="width80 border2">{name}</th>
+            <th className="width20 border2"></th>
           </tr>
         </thead>
         <tbody>
@@ -47,7 +71,7 @@ class Component extends React.Component {
         <div className="align-right">
           <span className="bold gray">
             Select Output Currency (Inputs Are Always in CAD):&nbsp;
-            <select className="bold input" onChange={this.handleCurrencyChange}>
+            <select className="bold input" onChange={this.handleCurrencyChange.bind(this)}>
               {
                 model.currency.getAllCurrencyNames().map(currency =>
                   <option value={currency} key={currency}>{currency}</option>)
@@ -60,12 +84,18 @@ class Component extends React.Component {
         <div>
           <div>
             <span className="bold output float-left">Net Worth</span>
-            <span className="bold output float-right">${this.state.netWorth}</span>
+            <span className="bold output float-right">
+              {
+                this.formatCurrency(this.state.netWorth, model.currency.getCurrency())
+              }
+            </span>
             <span className="clear"></span>
           </div>
         </div>
         <br></br>
         <hr></hr>
+
+        <br></br>
 
         <div>
           <span className="bold output">Assets</span>
@@ -83,6 +113,57 @@ class Component extends React.Component {
             this.getTable(model.longTermAssets, "Long Term Assets")
           }
         </div>
+
+        <div>
+          <div>
+            <span className="bold output float-left">Total Assets</span>
+            <span className="bold output float-right">
+              {
+                this.formatCurrency(this.state.totalAssets, model.currency.getCurrency())
+              }
+            </span>
+            <span className="clear"></span>
+          </div>
+        </div>
+        <br></br>
+        <hr></hr>
+
+        <br></br>
+        <br></br>
+
+        <div>
+          <span className="bold output">Liabilities</span>
+          <br></br>
+          <hr></hr>
+        </div>
+
+        <div>
+          {
+            this.getTable(model.shortTermLiabilities, "Short Term Liabilities")
+          }
+        </div>
+        <div>
+          {
+            this.getTable(model.longTermDebt, "Long Term Debt")
+          }
+        </div>
+
+        <div>
+          <div>
+            <span className="bold output float-left">Total Liabilities</span>
+            <span className="bold output float-right">
+              {
+                this.formatCurrency(this.state.totalLiabilities, model.currency.getCurrency())
+              }
+            </span>
+            <span className="clear"></span>
+          </div>
+        </div>
+        <br></br>
+        <hr></hr>
+
+        <br></br>
+        <br></br>
       </div>
     );
   }
